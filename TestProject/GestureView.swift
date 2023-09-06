2dfc56db8595f2ee5aa8a6eb234a8a71ef16ef16
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-06 11:03:26 +0300

    add gesture view

---
+++ TestProject/GestureView.swift
@@ -0,0 +1,20 @@
+//
+//  GestureView.swift
+//  TestProject
+//
+//  Created by Іван Штурхаль on 06.09.2023.
+//
+
+import UIKit
+
+class GestureView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
